edcdd9ca3e5041e9c1c3472d7415a62f8c8b6e68
vitor <wc_castro@hotmail.com>
2020-01-09 16:25:39 -0300

    Ok

---
+++ app/src/main/java/application/sysmar/prototipo/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package application.sysmar.prototipo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import application.sysmar.prototipo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
