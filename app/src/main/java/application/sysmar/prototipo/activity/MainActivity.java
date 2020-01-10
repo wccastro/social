820670e89ecd1a805f5e26ee5195b4dac3b9dacf
William Castro <wc_castro@hotmail.com>
2020-01-10 15:51:09 -0300

    william castro alteração layout

--- app/src/main/java/application/sysmar/prototipo/activity/MainActivity.java
+++ app/src/main/java/application/sysmar/prototipo/activity/MainActivity.java
@@ -2,11 +2,15 @@ package application.sysmar.prototipo.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import application.sysmar.prototipo.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Object View;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+    }
+    public void abrirTelaLogin(android.view.View  view){
+        startActivity( new Intent(this, LoginActivity.class));
+    }
+
+    public void abritTelaCadastro(android.view.View view){
+        startActivity( new Intent(this, CadastroActivity.class));
     }
 }
